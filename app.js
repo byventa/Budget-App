eb1e600db5be8d6945aa0087f5c5f63a21bddbde
byventa <patrykmaliglowka@icloud.com>
2020-06-25 03:27:12 +0200

    Added reading input type functionality

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
+///////////////////////////////////////////////////
 var budgetController = (function(){
-
 })();
-
-
+///////////////////////////////////////////////////
 var uiController = (function(){
-    
-})();
+    var domStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
 
+    };
+    return {
+        getInput: function(){
+            return{
+        inputType: '.add__type',
+                type: document.querySelector(domStrings.inputType).value,
+                description: document.querySelector(domStrings.inputDescription).value,
+                value: document.querySelector(domStrings.inputValue).value
+            };
+        },
+        getDOMstrings:function(){
+            return domStrings;
+        }
+    };
+})();
+///////////////////////////////////////////////////
 var appController = (function(uiCtrl,budgetCtrl){
-    
-})(budgetController,uiController);
+    var DOM = uiCtrl.getDOMstrings();
+    console.log(DOM);
+    var ctrlAddItem = function(){
+        var input = uiCtrl.getInput();
+        console.log(input);
+
+
+
+    };
+    document.querySelector(DOM.inputButton).addEventListener('click',ctrlAddItem);
+    document.addEventListener('keypress',function(event){
+        if(event.which === 13){ctrlAddItem();}
+    });
+})(uiController,budgetController);
